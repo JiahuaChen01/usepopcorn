9c43ba4221632b20d1e3747745595481d95a11f9
Jiahua Chen <jc2936@cornell.edu>
2024-11-29 16:14:55 -0500

    proptypes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 //import "./index.css";
 //import App from "./App";
 
-import StartRating from "./StarRating";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, SetMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={SetMovieRating} />
+      <p>This movies was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* App / */}
-    <StartRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+
+    <Test />
   </React.StrictMode>
 );
